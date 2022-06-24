04cee55c927baaa744d1392406b19f597d8c82c4
TiveCS <creativecode.id@gmail.com>
2022-06-24 09:49:50 +0700

    Add blueprint

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import openpyxl
 
+dataset_file = "DataSetTB3_SHARE S1 RPL.xlsx"
+data = {}
+
+def read_file():
+    pass
+
+def write_file():
+    pass
+
+def data_processing():
+    pass
+
+def calculate_distance():
+    pass
+
+def find_k_value():
+    pass
+
+def classification():
+    pass
+
 if __name__ == '__main__':
     print('Hello world')
